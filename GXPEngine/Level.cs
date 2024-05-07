b0eba9fbd695100480e609fd811b91e1c5243eb1
Julius Struijk <julius.struijk@gmail.com>
2024-05-07 17:13:20 +0200

    Fixed exception
    Fixed a exception in the Level parameters that was caused by an imperfect merge.

--- GXPEngine/Level.cs
+++ GXPEngine/Level.cs
@@ -15,7 +15,7 @@ class Level : GameObject
 
     int borderLenght = 585;
     int borderWidth = 35;
-    public Level(float pX, float pY, int pBoundarySize) : base(true)
+    public Level(Vec2 pPosition, int pBoundarySize) : base(true)
     {
         boundarySize = pBoundarySize;
         position = pPosition;
--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -14,7 +14,7 @@ public class MyGame : Game {
     public MyGame() : base(1920, 1080, false,false,-1,-1,true)
 	{
         targetFps = 60; // Consistent, non variable framerate
-        AddChild(new Level(width / 2, height / 2, 300));
+        AddChild(new Level(new Vec2(width / 2, height / 2), 300));
 
         cogWheel = new CogWheel(new Vec2(width/2, height/2), 10);
         AddChild(cogWheel);
