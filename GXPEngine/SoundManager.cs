c451de291447777878f7803f0841b2f73614a38e
Aleksandar <549190@student.saxion.nl>
2024-05-08 13:25:02 +0200

    Sound Manager

---
+++ GXPEngine/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GXPEngine;
+internal class SoundManager
+{
+    public static SoundManager test = new SoundManager("test.wav", 1 , 1);
+
+    private Sound storedSound;
+    private float defaultVolume;
+    private uint defaultChannel;
+
+    public SoundManager(String fileName, float defaultVolume, float defaultChannel)
+    {
+        storedSound = new Sound(fileName);
+        this.defaultVolume = defaultVolume;
+        this.defaultChannel = this.defaultChannel;
+    }
+
+    public void play(float volume, uint chanel)
+    {
+        storedSound.Play(false, chanel, volume, 0);
+    }
+}
